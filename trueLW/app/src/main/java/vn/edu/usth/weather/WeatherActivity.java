f4c98236d4e071c393e5e3eeee8a88ec4d751fc0
Erenanhai <namvtp.bi9022@st.usth.edu.vn>
2021-10-06 14:00:26 +0700

    adding a WIP pw3 in trueLW until it's fully functional

--- trueLW/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ trueLW/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -10,6 +10,11 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        // Create a new Fragment to be placed in the activity layout
+        DetailFragment firstFragment = new DetailFragment();
+        // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
     }
 
     @Override
