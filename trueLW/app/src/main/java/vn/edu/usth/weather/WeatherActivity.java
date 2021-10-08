645f8908aa3475d3f86e421075583dfea753e401
Erenanhai <namvtp.bi9022@st.usth.edu.vn>
2021-10-08 09:27:29 +0700

    Fully completed pw2 while still struggle with pw3 and pw4 (at least it's progress) :v

--- trueLW/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ trueLW/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -3,6 +3,7 @@ package vn.edu.usth.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -10,35 +11,41 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        Log.i("tag", "Created");
 
         // Create a new Fragment to be placed in the activity layout
-        DetailFragment firstFragment = new DetailFragment();
+        //ForecastFragment Forcast = new ForecastFragment();
         // Add the fragment to the 'container' FrameLayout
-        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+        //getSupportFragmentManager().beginTransaction().add(R.id.container, Forcast).commit();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.i("tag","Started");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.i("tag", "Stopped");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.i("tag", "Oh, I'm die. Thank you forever.");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.i("tag", "Paused");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.i("tag", "Continue");
     }
 }
